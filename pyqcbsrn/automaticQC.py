d7aa4bff118667b720265c7a93baf13d920b885b
mbaronnet <maelle.baronnet@mines-paristech.fr>
2021-07-09 10:00:18 +0400

    Add of modules and configuration

---
+++ pyqcbsrn/automaticQC.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import csv
+import json
+import os
+import sys
+import pandas as pd
+import pvlib
+import datetime
+from collections import OrderedDict
+from pathlib import Path
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.plotting import figure, output_file, show
+from qclib import qcrad, utils
+
+# Get data conf from JSON file
+with open('conf/autoqc_conf.json', 'r') as f:
+    loaded_json = json.load(f)
+
+
+class conf:
+    """
+    BSRN station, database and headers from input file configurations
+    """
+    LAT = loaded_json['station']['LAT']
+    LON = loaded_json['station']['LON']
+    ALT = loaded_json['station']['ALT']
+    TZ = loaded_json['station']['TZ']
+    TIMESTAMP_NAME = loaded_json['header']['TIMESTAMP_NAME']
+    GSW_NAME = loaded_json['header']['GSW_NAME']
+    DIF_NAME = loaded_json['header']['DIF_NAME']
+    DIR_NAME = loaded_json['header']['DIR_NAME']
+    LWDN_NAME = loaded_json['header']['LWDN_NAME']
+    TA_NAME = loaded_json['header']['TA_NAME']
+
+
+def fix_values(row: OrderedDict):
+    GSW, Dif, DirN, LWdn, Ta, Td = None, None, None, None, None, None
+    # check variables name on input file
+    try:
+        timestamp = row[conf.TIMESTAMP_NAME]
+        if conf.GSW_NAME in row and utils.isfloat(row[conf.GSW_NAME]):
+            GSW = float(row[conf.GSW_NAME])
+        if conf.DIF_NAME in row and utils.isfloat(row[conf.DIF_NAME]):
+            Dif = float(row[conf.DIF_NAME])
+        if conf.DIR_NAME in row and utils.isfloat(row[conf.DIR_NAME]):
+            DirN = float(row[conf.DIR_NAME])
+        if conf.LWDN_NAME in row and utils.isfloat(row[conf.LWDN_NAME]):
+            LWdn = float(row[conf.LWDN_NAME])
+        if conf.TA_NAME in row and utils.isfloat(row[conf.TA_NAME]):
+            Ta = float(row[conf.TA_NAME]) + 273.15
+    except KeyError as e:
+        print('KeyError: %s' %str(e))
+    except ValueError as valueError:
+        print('ValueError: %s' %str(valueError))
+    return (timestamp, GSW, Dif, DirN, LWdn, Ta)
+
+
+def getRow(row: OrderedDict, zenith_serie):
+    # get values of parameters
+    [timestamp, GSW, Dif, DirN, LWdn, Ta] = fix_values(row)
+    SZA = float(zenith_serie[timestamp])
+    # application of data quality control
+    qc_result = {
+        "timestamp" : timestamp,
+        "QC1"  : qcrad.QC1(GSW, SZA),
+        "QC2"  : qcrad.QC2(Dif, SZA),
+        "QC3"  : qcrad.QC3(DirN, SZA),
+        "QC5"  : qcrad.QC5(LWdn),
+        "QC7"  : qcrad.QC7(GSW, Dif, DirN, SZA),
+        "QC8"  : qcrad.QC8(Dif, GSW, SZA),
+        "QC10" : qcrad.QC10(LWdn, Ta),
+        "QC19" : qcrad.QC19(Ta)
+    }
+    # create row for aqc file
+    row_aqc = row
+    for key in row_aqc.keys():
+        if key != "timestamp":
+            row_aqc[key] = 0
+    if qc_result["QC1"] > 2:
+        row_aqc["global2_avg"] = 1
+        row_aqc["global2_std"] = 1
+        row_aqc["global2_min"] = 1
+        row_aqc["global2_max"] = 1
+    if qc_result["QC2"] > 2:
+        row_aqc["diffuse_avg"] = 1
+        row_aqc["diffuse_std"] = 1
+        row_aqc["diffuse_min"] = 1
+        row_aqc["diffuse_max"] = 1
+    if qc_result["QC3"] > 2:
+        row_aqc["direct_avg"] = 1
+        row_aqc["direct_std"] = 1
+        row_aqc["direct_min"] = 1
+        row_aqc["direct_max"] = 1
+    if qc_result["QC5"] > 2:
+        row_aqc["downward_avg"] = 1
+        row_aqc["downward_std"] = 1
+        row_aqc["downward_min"] = 1
+        row_aqc["downward_max"] = 1
+    if qc_result["QC19"] > 0:
+        row_aqc["temperature"] = 1
+    # create row for qcrad file
+    row_qcrad = qc_result
+    return row_aqc, row_qcrad
+
+
+def generateQCFiles(filepath):
+    """Create the 2 files _aqc.csv and _qrcad.csv"""
+    # Manage filenames
+    FILE_BRUT = filepath[0]
+    FILE_AQC = os.path.splitext(FILE_BRUT.replace('_brut', ''))[0]+'_aqc.csv'
+    FILE_QCRAD = os.path.splitext(FILE_BRUT.replace('_brut', ''))[0]+'_qcrad.csv'
+    # load input file into a DataFrame
+    file_brut = pd.read_csv(FILE_BRUT)
+    timestamp_list = file_brut.timestamp.to_list()
+    zenith_serie = utils.getZenith(timestamp_list, conf.LAT, conf.LON, conf.ALT)
+    # process input file
+    datapoints_qcrad, datapoints_aqc = [], []
+    with open(FILE_BRUT, 'r') as fileIn:
+        reader = csv.DictReader(fileIn, delimiter=',')
+        for row in reader:
+            (row_aqc, row_qcrad) = getRow(row, zenith_serie)
+            datapoints_qcrad.append(row_qcrad)
+            datapoints_aqc.append(row_aqc)
+    # convert list of qcrad results into dataframe & save as a csv file
+    df_qcrad = pd.DataFrame(datapoints_qcrad)
+    df_qcrad.to_csv(FILE_QCRAD, index=False)
+    print("Successfully saved CSV file in", FILE_QCRAD)
+    # convert list of aqc results into dataframe & save as a csv file
+    df_aqc = pd.DataFrame(datapoints_aqc)
+    df_aqc.to_csv(FILE_AQC, index=False)
+    print("Successfully saved CSV file in", FILE_AQC)
+
+
+def plotQCFiles(filepath):
+    """Plot the input file"""
+    # init
+    csv_name = filepath[0]
+    yearMonth = (((os.path.splitext((csv_name))[0]).split('/'))[-1]).split('_')[0]
+    typeFile = (((os.path.splitext((csv_name))[0]).split('/'))[-1]).split('_')[1]
+    # create directory if it necessary
+    Path("plot/"+yearMonth).mkdir(parents=True, exist_ok=True)
+    chart_name = 'plot/'+yearMonth+'/'+typeFile+'.html'
+    if typeFile == "qcrad":
+        parameters_to_plot = ["QC1", "QC2", "QC3", "QC5", "QC7", "QC8", "QC10", "QC19"]
+    else:
+        parameters_to_plot = ["global2_avg", "diffuse_avg", "direct_avg", "downward_avg", "temperature"]
+    # get sensor data
+    df = pd.read_csv(csv_name)
+    timestamp = df.timestamp.to_list()
+    date = []
+    for time in timestamp:
+        date.append(datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S'))
+    df.insert(1, "date", date)
+    source = ColumnDataSource(df)
+    # create plots and choose color
+    plots = []
+    for var in parameters_to_plot:
+        hover = HoverTool()
+        s = figure(plot_width=1600, plot_height=900, x_axis_type="datetime")
+        s.circle(x='date', y=var, source=source)
+        s.line(x='date', y=var, legend_label=var, source=source)
+        hover.tooltips=[
+            ('date', '@date{%F %T}'),
+            (var, '@'+var)
+        ]
+        hover.formatters = {'@date': 'datetime'}
+        s.add_tools(hover)
+        plots.append(s)
+    # show and save plot
+    output_file(chart_name)
+    print("Successfully saved HTML file in", chart_name)
+    show(column(plots))
---
+++ pyqcbsrn/qc_functions.py
@@ -0,0 +1,450 @@
+"""
+Module that provides the QC class with their assiocated functions and labeling
+"""
+
+
+# Imports required
+import math
+import numpy as np
+
+# Physics variables
+
+
+class ref:
+    """BSRN Various quantities related to measurements"""
+    Tsnw = None  # temperature limit for albedo limit test, temp at which "snow" limit is used
+    SOLAR_CONSTANT = 1368  # solar constant at mean Earth-Sun distance uses by QCRad in W.m-2
+    BOLTZMANN = 5.67e-8  # Stephan-Boltzman constant = 5.67 x 10 -8 Wm -2 K -4
+
+
+"""
+Flag Value: Related to Type test:
+5-6 Global Physical Limits (PP)
+3-4 User configurable (UC2) 2nd level tests, also LW Tc and Td tests
+1-2 User configurable (UC1) 1st level tests and non-definitive tests
+0   No test failures
+-1  Missing data or test not possible
+"""
+
+
+''' *********** QC1-QC6 (GSW, Diffuse SW, Direct SW, SWup, LWdn and LWup basic limits tests) *********** '''
+
+# Definition of the QC class
+
+
+class QC1:
+
+    def __init__(self):
+        self.name = 'QC1'
+        self.varx = 'SZA'
+        self.vary = 'global2_avg'
+        self.unitx = 'SZA'
+        self.unity = 'W.m-2'
+        self.coefficients = {'level_1': 'C1', 'level_2': 'D1'}
+        self.coef_range = [0.0, 1.2]
+
+    def f(self, SZA, GSW, coefs):
+        ''' Return the 2 main variables, the 1rst level limit, the 2nd level limit and the physical limit for a sample'''
+
+        l1 = ref.SOLAR_CONSTANT * coefs['BSRN']['C1'] * math.pow(math.cos(math.radians(SZA)), 1.2) + 50
+        l2 = ref.SOLAR_CONSTANT * coefs['BSRN']['D1'] * math.pow(math.cos(math.radians(SZA)), 1.2) + 55
+        l_bsrn = ref.SOLAR_CONSTANT * 1.5 * math.pow(math.cos(math.radians(SZA)), 1.2) + 100
+
+        return l1, l2, l_bsrn
+
+    def lab(self, SZA, GSW, coefs):
+        """GSW [basic limits tests]"""
+        if GSW is not None:
+            if (GSW < -4):
+                return 5
+            if (GSW < -2):
+                return 3
+            if SZA >= 0 and SZA <= 90:  # cannot have float exponent of negative value
+                if(GSW > (ref.SOLAR_CONSTANT * 1.5 * math.pow(math.cos(math.radians(SZA)), 1.2)) + 100):
+                    return 6
+                if(GSW > (ref.SOLAR_CONSTANT * coefs['BSRN']['D1'] * math.pow(math.cos(math.radians(SZA)), 1.2)) + 55):
+                    return 4
+                if(GSW > (ref.SOLAR_CONSTANT * coefs['BSRN']['C1'] * math.pow(math.cos(math.radians(SZA)), 1.2)) + 50):
+                    return 2
+            else:
+                return -1
+        else:
+            return -1
+        return 0
+
+    def calc_lim(self, df, coefs):
+        """ Computation of the limits and the labels of a dataframe"""
+
+        df = df[['timestamp', self.varx, self.vary]]
+
+        X_val1 = np.array(df[self.varx])
+        X_val2 = np.array(df[self.vary])
+
+        lim_l1 = []
+        lim_l2 = []
+        lim_bsrn = []
+
+        lim_l1_min = []
+        lim_l2_min = []
+        lim_bsrn_min = []
+
+        labels = []
+
+        for VAL1, VAL2 in zip(X_val1, X_val2):
+
+            if self.vary == 'downward_avg':
+
+                l1, l2, l_bsrn, l1_min, l2_min, l_bsrn_min = self.f(VAL1, VAL2, coefs)
+
+                lim_l1_min.append(l1_min)
+                lim_l2_min.append(l2_min)
+                lim_bsrn_min.append(l_bsrn_min)
+
+            else:
+
+                l1, l2, l_bsrn = self.f(VAL1, VAL2, coefs)
+
+            label = self.lab(VAL1, VAL2, coefs)
+
+            lim_l1.append(l1)
+            lim_l2.append(l2)
+            lim_bsrn.append(l_bsrn)
+            labels.append(label)
+
+        return(lim_l1, lim_l2, lim_bsrn, labels)
+
+
+class QC2:
+
+    def __init__(self):
+        self.name = 'QC2'
+        self.varx = 'SZA'
+        self.vary = 'diffuse_avg'
+        self.unitx = 'SZA'
+        self.unity = 'W.m-2'
+        self.coefficients = {'level_1': 'C2', 'level_2': 'D2'}
+        self.coef_range = [0.0, 1.0]
+
+    def f(self, SZA, Dif, coefs):
+        ''' Return the 2 main variables, the 1rst level limit, the 2nd level limit and the physical limit for a sample'''
+
+        l1 = ref.SOLAR_CONSTANT * coefs['BSRN']['C2'] * math.pow(math.cos(math.radians(SZA)), 1.2) + 30
+        l2 = ref.SOLAR_CONSTANT * coefs['BSRN']['D2'] * math.pow(math.cos(math.radians(SZA)), 1.2) + 35
+        l_bsrn = ref.SOLAR_CONSTANT * 0.95 * math.pow(math.cos(math.radians(SZA)), 1.2) + 50
+
+        return l1, l2, l_bsrn
+
+    def lab(self, SZA, Dif, coefs):
+        """Diffuse SW [basic limits tests]"""
+        if Dif is not None:
+            if (Dif < -4):
+                return 5
+            if(Dif < -2):
+                return 3
+            if SZA >= 0 and SZA <= 90:  # cannot have float exponent of negative value
+                if(Dif > (ref.SOLAR_CONSTANT * 0.95 * math.pow(math.cos(math.radians(SZA)), 1.2)) + 50):
+                    return 6
+                if(Dif > (ref.SOLAR_CONSTANT * coefs['BSRN']['D2'] * math.pow(math.cos(math.radians(SZA)), 1.2)) + 35):
+                    return 4
+                if(Dif > (ref.SOLAR_CONSTANT * coefs['BSRN']['C2'] * math.pow(math.cos(math.radians(SZA)), 1.2)) + 30):
+                    return 2
+            else:
+                return -1
+        else:
+            return -1
+        return 0
+
+    def calc_lim(self, df, coefs):
+        """ Computation of the limits and the labels of a dataframe"""
+
+        df = df[['timestamp', self.varx, self.vary]]
+
+        X_val1 = np.array(df[self.varx])
+        X_val2 = np.array(df[self.vary])
+
+        lim_l1 = []
+        lim_l2 = []
+        lim_bsrn = []
+
+        lim_l1_min = []
+        lim_l2_min = []
+        lim_bsrn_min = []
+
+        labels = []
+
+        for VAL1, VAL2 in zip(X_val1, X_val2):
+
+            if self.vary == 'downward_avg':
+
+                l1, l2, l_bsrn, l1_min, l2_min, l_bsrn_min = self.f(VAL1, VAL2, coefs)
+
+                lim_l1_min.append(l1_min)
+                lim_l2_min.append(l2_min)
+                lim_bsrn_min.append(l_bsrn_min)
+
+            else:
+
+                l1, l2, l_bsrn = self.f(VAL1, VAL2, coefs)
+
+            label = self.lab(VAL1, VAL2, coefs)
+
+            lim_l1.append(l1)
+            lim_l2.append(l2)
+            lim_bsrn.append(l_bsrn)
+            labels.append(label)
+
+        return(lim_l1, lim_l2, lim_bsrn, labels)
+
+
+class QC3:
+
+    def __init__(self):
+        self.name = 'QC3'
+        self.varx = 'SZA'
+        self.vary = 'direct_avg'
+        self.unitx = 'SZA'
+        self.unity = 'W.m-2'
+        self.coefficients = {'level_1': 'C3', 'level_2': 'D3'}
+        self.coef_range = [0.0, 1.0]
+
+    def f(self, SZA, DirN, coefs):
+        ''' Return the 2 main variables, the 1rst level limit, the 2nd level limit and the physical limit for a sample'''
+
+        l1 = ref.SOLAR_CONSTANT * coefs['BSRN']['C3'] * math.pow(math.cos(math.radians(SZA)), 0.2) + 10
+        l2 = ref.SOLAR_CONSTANT * coefs['BSRN']['D3'] * math.pow(math.cos(math.radians(SZA)), 0.2) + 15
+        l_bsrn = ref.SOLAR_CONSTANT
+
+        return l1, l2, l_bsrn
+
+    def lab(self, SZA, DirN, coefs):
+        """Direct Normal SW  [basic limits tests]"""
+        if DirN is not None:
+            if (DirN < -4):
+                return 5
+            if(DirN < -2):
+                return 3
+            if (DirN > ref.SOLAR_CONSTANT):
+                return 6
+            if SZA >= 0 and SZA <= 90:  # cannot have float exponent of negative value
+                if(DirN > (ref.SOLAR_CONSTANT * coefs['BSRN']['D3'] * math.pow(math.cos(math.radians(SZA)), 0.2)) + 15):
+                    return 4
+                if(DirN > (ref.SOLAR_CONSTANT * coefs['BSRN']['C3'] * math.pow(math.cos(math.radians(SZA)), 0.2)) + 10):
+                    return 2
+            else:
+                return -1
+        else:
+            return -1
+        return 0
+
+    def calc_lim(self, df, coefs):
+        """ Computation of the limits and the labels of a dataframe"""
+
+        df = df[['timestamp', self.varx, self.vary]]
+
+        X_val1 = np.array(df[self.varx])
+        X_val2 = np.array(df[self.vary])
+
+        lim_l1 = []
+        lim_l2 = []
+        lim_bsrn = []
+
+        lim_l1_min = []
+        lim_l2_min = []
+        lim_bsrn_min = []
+
+        labels = []
+
+        for VAL1, VAL2 in zip(X_val1, X_val2):
+
+            if self.vary == 'downward_avg':
+
+                l1, l2, l_bsrn, l1_min, l2_min, l_bsrn_min = self.f(VAL1, VAL2, coefs)
+
+                lim_l1_min.append(l1_min)
+                lim_l2_min.append(l2_min)
+                lim_bsrn_min.append(l_bsrn_min)
+
+            else:
+
+                l1, l2, l_bsrn = self.f(VAL1, VAL2, coefs)
+
+            label = self.lab(VAL1, VAL2, coefs)
+
+            lim_l1.append(l1)
+            lim_l2.append(l2)
+            lim_bsrn.append(l_bsrn)
+            labels.append(label)
+
+        return(lim_l1, lim_l2, lim_bsrn, labels)
+
+
+class QC5:
+
+    def __init__(self):
+        self.name = 'QC5'
+        self.varx = 'SZA'
+        self.vary = 'downward_avg'
+        self.unitx = 'SZA'
+        self.unity = 'W.m-2'
+        self.coefficients = {'level_1_min': 'C5', 'level_2_min': 'D5', 'level_1': 'C6', 'level_2': 'D6'}
+        self.coef_range = [400.0, 600.0]
+        self.coef_range_min = [200.0, 400.0]
+
+    def f(self, SZA, LWdn, coefs):
+        ''' Return the 2 main variables, the 1rst level limit, the 2nd level limit and the physical limit for a sample'''
+
+        l1_max = coefs['BSRN']['C6']
+        l2_max = coefs['BSRN']['D6']
+        l_bsrn_max = 700
+
+        l1_min = coefs['BSRN']['C5']
+        l2_min = coefs['BSRN']['D5']
+        l_bsrn_min = 40
+
+        return l1_max, l2_max, l_bsrn_max, l1_min, l2_min, l_bsrn_min
+
+    def lab(self, Ta, LWdn, coefs):
+        """LWdn [basic limits tests]"""
+        if LWdn is not None:
+            if (LWdn > 700):
+                return 6
+            if (LWdn < 40):
+                return 5
+            if (LWdn > coefs['BSRN']['D6']):
+                return 4
+            if (LWdn < coefs['BSRN']['D5']):
+                return 3
+            if (LWdn > coefs['BSRN']['C6']):
+                return 2
+            if (LWdn < coefs['BSRN']['C5']):
+                return 1
+        else:
+            return -1
+        return 0
+
+    def calc_lim(self, df, coefs):
+        """ Computation of the limits and the labels of a dataframe"""
+
+        df = df[['timestamp', self.varx, self.vary]]
+
+        X_val1 = np.array(df[self.varx])
+        X_val2 = np.array(df[self.vary])
+
+        lim_l1 = []
+        lim_l2 = []
+        lim_bsrn = []
+
+        lim_l1_min = []
+        lim_l2_min = []
+        lim_bsrn_min = []
+
+        labels = []
+
+        for VAL1, VAL2 in zip(X_val1, X_val2):
+
+            l1, l2, l_bsrn, l1_min, l2_min, l_bsrn_min = self.f(VAL1, VAL2, coefs)
+
+            lim_l1_min.append(l1_min)
+            lim_l2_min.append(l2_min)
+            lim_bsrn_min.append(l_bsrn_min)
+
+            label = self.lab(VAL1, VAL2, coefs)
+
+            lim_l1.append(l1)
+            lim_l2.append(l2)
+            lim_bsrn.append(l_bsrn)
+            labels.append(label)
+
+        return(lim_l1, lim_l2, lim_bsrn, labels)
+
+
+class QC10:
+
+    def __init__(self):
+        self.name = 'QC10'
+        self.varx = 'temperature'
+        self.vary = 'downward_avg'
+        self.unitx = '°K'
+        self.unity = 'W.m-2'
+        self.coefficients = {'level_1_min': 'C11', 'level_2_min': 'D11', 'level_1': 'C12', 'level_2': 'D12'}
+        self.coef_range = [0.0, 10.0]
+        self.coef_range_min = [0.60, 1.0]
+
+    def f(self, Ta, LWdn, coefs):
+        ''' Return the 2 main variables, the 1rst level limit, the 2nd level limit and the physical limit for a sample'''
+
+        l1_max = ref.BOLTZMANN * math.pow(Ta + 273.15, 4) + coefs['BSRN']['C12']
+        l2_max = ref.BOLTZMANN * math.pow(Ta + 273.15, 4) + coefs['BSRN']['D12']
+        l_bsrn_max = 700
+
+        l1_min = coefs['BSRN']['C11'] * ref.BOLTZMANN * math.pow(Ta + 274.15, 4)
+        l2_min = coefs['BSRN']['D11'] * ref.BOLTZMANN * math.pow(Ta + 274.15, 4)
+        l_bsrn_min = 40
+
+        return l1_max, l2_max, l_bsrn_max, l1_min, l2_min, l_bsrn_min
+
+    def lab(self, Ta, LWdn, coefs):
+        """LWdn to Ta test"""
+        if all(v is not None for v in [LWdn, Ta]):
+            if QC19(Ta) == 0:
+                if (LWdn > (ref.BOLTZMANN * math.pow(Ta, 4) + coefs['BSRN']['D12'])):
+                    return 4
+                if (LWdn < (coefs['BSRN']['D11'] * ref.BOLTZMANN * math.pow(Ta, 4))):
+                    return 3
+                if (LWdn > (ref.BOLTZMANN * math.pow(Ta, 4) + coefs['BSRN']['C12'])):
+                    return 2
+                if (LWdn < (coefs['BSRN']['C11'] * ref.BOLTZMANN * math.pow(Ta, 4))):
+                    return 1
+            else:
+                return -1
+        else:
+            return -1
+        return 0
+
+    def calc_lim(self, df, coefs):
+        """ Computation of the limits and the labels of a dataframe"""
+
+        df = df[['timestamp', self.varx, self.vary]]
+
+        X_val1 = np.array(df[self.varx]) + 273.15
+        X_val2 = np.array(df[self.vary])
+
+        lim_l1 = []
+        lim_l2 = []
+        lim_bsrn = []
+
+        lim_l1_min = []
+        lim_l2_min = []
+        lim_bsrn_min = []
+
+        labels = []
+
+        for VAL1, VAL2 in zip(X_val1, X_val2):
+
+            l1, l2, l_bsrn, l1_min, l2_min, l_bsrn_min = self.f(VAL1, VAL2, coefs)
+
+            lim_l1_min.append(l1_min)
+            lim_l2_min.append(l2_min)
+            lim_bsrn_min.append(l_bsrn_min)
+
+            label = self.lab(VAL1, VAL2, coefs)
+
+            lim_l1.append(l1)
+            lim_l2.append(l2)
+            lim_bsrn.append(l_bsrn)
+            labels.append(label)
+
+        return(lim_l1, lim_l2, lim_bsrn, labels)
+
+# -------------------------------------------------------------------------------------------
+
+
+def QC19(Ta):
+    """Ta testing"""
+    if Ta is not None:
+        if Ta > 350 or Ta < 170:
+            return 1
+    else:
+        print('Eliminé dès le premier test')
+        return -1
+    return 0
